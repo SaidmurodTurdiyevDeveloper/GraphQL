e1553dfa1b21bd18c6f18bd7ad8725a96bb12da5
SaidmurodTurdiyevDeveloper <98685816+SaidmurodTurdiyevDeveloper@users.noreply.github.com>
2022-10-23 17:35:17 +0500

    first commit

---
+++ app/src/main/java/com/example/graphql/FirstFragment.kt
@@ -0,0 +1,83 @@
+package com.example.graphql
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.os.Looper
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.network.okHttpClient
+import com.aymen.graphql.CharactersListQuery
+import com.example.graphql.databinding.FragmentFirstBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val TAG = "FirstFragment"
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        check(Looper.myLooper() == Looper.getMainLooper()) {
+            "Only the main thread can get the apolloClient instance"
+        }
+
+        val okHttpClient = OkHttpClient.Builder().build()
+        val clent = ApolloClient.Builder()
+            .serverUrl("https://rickandmortyapi.com/graphql")
+            .okHttpClient(okHttpClient)
+            .build()
+        lifecycleScope.launch(Dispatchers.IO) {
+            val result = clent.query(CharactersListQuery()).execute()
+            result.data?.let { data ->
+                val list = data.characters?.results
+                val text =  StringBuilder()
+                withContext(Dispatchers.Main) {
+                    list?.forEach { character ->
+                        Log.d(TAG, "onViewCreated: " + character.toString())
+                        character?.let {
+                            Log.d(TAG, "onViewCreated: " + it.toString())
+                            text.append( it.id.toString() + " " + it.name.toString() + " " + it.species.toString())
+                            text.append("\n")
+                        }
+                    }
+                    binding.textviewFirst.text = text
+                    Log.d(TAG, text.toString())
+                }
+            }
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
